a4cf03393a372c7745a5c0c6a102efa2240b368c
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-06 09:31:12 +0200

    Adding template

--- API/src/main/java/dev/sdkssh/gameapi/api/properties/Property.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/properties/Property.java
@@ -2,13 +2,14 @@ package dev.sdkssh.gameapi.api.properties;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 
 public class Property<T> {
     @Getter
     @Setter
     private T value;
 
-    public Property(T value){
+    public Property(@NotNull T value){
         this.value = value;
     }
 }
---
+++ API/src/main/java/dev/sdkssh/gameapi/api/templates/Template.java
@@ -0,0 +1,7 @@
+package dev.sdkssh.gameapi.api.templates;
+
+public abstract class Template {
+    public abstract void onLoad();
+
+    public abstract void onUnload();
+}
