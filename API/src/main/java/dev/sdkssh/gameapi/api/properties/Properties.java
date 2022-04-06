05a4098c6020bd7a5272097c29de54bf1f043b3a
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-06 16:59:45 +0200

    Set depracted multiarena because it will be soon

--- API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
@@ -2,6 +2,7 @@ package dev.sdkssh.gameapi.api.properties;
 
 public class Properties {
     public static Property<Integer> PLAYERS = new Property<>(6);
+    @Deprecated
     public static Property<Boolean> MULTIARENA = new Property<>(false);
     public static Property<String> NAME = new Property<>("Game");
     public static Property<Boolean> BUNGEE = new Property<>(false);
