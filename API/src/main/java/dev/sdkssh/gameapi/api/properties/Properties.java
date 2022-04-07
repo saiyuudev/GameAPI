7f996ecd1590ab751dbb2f8a2e4b70d9743d4220
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 17:38:10 +0200

    Remove some properties

--- API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
@@ -2,11 +2,6 @@ package dev.sdkssh.gameapi.api.properties;
 
 public class Properties {
     public static Property<Integer> PLAYERS = new Property<>(6);
-    @Deprecated
-    public static Property<Boolean> MULTIARENA = new Property<>(false);
-    public static Property<Boolean> BUNGEE = new Property<>(false);
-    public static Property<String> BUNGEE_SERVER = new Property<>("");
-    public static Property<Boolean> TEAMS = new Property<>(false);
     public static Property<Integer> PLAYERS_START = new Property<>(2);
     public static Property<Integer> PLAYERS_FORCE_START = new Property<>(6);
     public static Property<Integer> START_TIMER = new Property<>(30);
