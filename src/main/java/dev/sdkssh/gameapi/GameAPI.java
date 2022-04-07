413bd193c2efeb01590868f9f19dd9d6e1829ae3
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 15:20:30 +0200

    Adding timer to on join

--- src/main/java/dev/sdkssh/gameapi/GameAPI.java
+++ src/main/java/dev/sdkssh/gameapi/GameAPI.java
@@ -1,11 +1,15 @@
 package dev.sdkssh.gameapi;
 
 import dev.sdkssh.gameapi.events.ConnectionEvents;
+import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GameAPI extends JavaPlugin {
+    @Getter
+    private static GameAPI plugin;
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new ConnectionEvents(), this);
+        plugin = this;
     }
 }
--- src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
+++ src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
@@ -1,16 +1,25 @@
 package dev.sdkssh.gameapi.events;
 
+import dev.sdkssh.gameapi.GameAPI;
+import dev.sdkssh.gameapi.api.properties.Properties;
+import dev.sdkssh.gameapi.api.templates.game.GameTemplate;
 import dev.sdkssh.gameapi.register.GameRegister;
+import dev.sdkssh.gameapi.schedulers.TimerTask;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConnectionEvents implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
         if(GameRegister.isRegistered()){
             GameRegister.getGame().addPlayer(e.getPlayer());
+            GameTemplate game = GameRegister.getGame();
+            if(Properties.PLAYERS_START.getValue() == game.getPlayers().size() && !TimerTask.isStart()){
+                new TimerTask(Properties.START_TIMER.getValue()).runTaskTimer(GameAPI.getPlugin(), 0, 20);
+            }
         }
     }
 
