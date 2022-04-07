6eae58b4d73e90829dca61b6b12bd4d9ac1b533d
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 17:37:48 +0200

    Adding kick when game is full or in game

--- API/src/main/java/dev/sdkssh/gameapi/api/message/Messages.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/message/Messages.java
@@ -3,4 +3,6 @@ package dev.sdkssh.gameapi.api.message;
 public class Messages {
     public static String TIMER_TITLE = "Start in";
     public static String TIMER_SUBTITLE = "%timer% seconds !";
+    public static String KICK_FULL = "This game is full !";
+    public static String KICK_INGAME = "This game is already started !";
 }
--- src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
+++ src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
@@ -1,18 +1,34 @@
 package dev.sdkssh.gameapi.events;
 
 import dev.sdkssh.gameapi.GameAPI;
+import dev.sdkssh.gameapi.api.message.Messages;
 import dev.sdkssh.gameapi.api.properties.Properties;
 import dev.sdkssh.gameapi.api.states.GameSTATE;
 import dev.sdkssh.gameapi.api.templates.game.GameTemplate;
 import dev.sdkssh.gameapi.api.templates.player.GamePlayerTemplate;
 import dev.sdkssh.gameapi.register.GameRegister;
 import dev.sdkssh.gameapi.schedulers.TimerTask;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionEvents implements Listener {
+    @EventHandler
+    public void onLogin(PlayerLoginEvent e){
+        if(GameRegister.isRegistered()) {
+            if(GameRegister.getGame().getState() != GameSTATE.LOBBY){
+                e.disallow(PlayerLoginEvent.Result.KICK_OTHER, Messages.KICK_INGAME);
+                return;
+            }
+            if(Bukkit.getOnlinePlayers().size() > Properties.PLAYERS.getValue()) {
+                e.disallow(PlayerLoginEvent.Result.KICK_OTHER, Messages.KICK_FULL);
+                return;
+            }
+        }
+    }
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
         if(GameRegister.isRegistered()){
