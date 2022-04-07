76bd4e9ab64fe1124554e7e2730d3115a6cec026
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 15:47:33 +0200

    Remove unused import

--- src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
+++ src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
@@ -7,12 +7,10 @@ import dev.sdkssh.gameapi.api.templates.game.GameTemplate;
 import dev.sdkssh.gameapi.api.templates.player.GamePlayerTemplate;
 import dev.sdkssh.gameapi.register.GameRegister;
 import dev.sdkssh.gameapi.schedulers.TimerTask;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class ConnectionEvents implements Listener {
     @EventHandler
