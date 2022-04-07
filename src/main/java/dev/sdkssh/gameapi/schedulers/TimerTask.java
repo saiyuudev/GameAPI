2c7e2f741a04ebe1b0cf7d7121116171690db732
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 15:15:04 +0200

    Adding timer task callback

--- src/main/java/dev/sdkssh/gameapi/schedulers/TimerTask.java
+++ src/main/java/dev/sdkssh/gameapi/schedulers/TimerTask.java
@@ -1,11 +1,39 @@
 package dev.sdkssh.gameapi.schedulers;
 
+import dev.sdkssh.gameapi.api.message.Messages;
+import dev.sdkssh.gameapi.api.templates.player.GamePlayerTemplate;
+import dev.sdkssh.gameapi.register.GameRegister;
+import dev.sdkssh.gameapi.utils.chat.TitleAPI;
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class TimerTask extends BukkitRunnable {
+    @Getter
+    @Setter
+    private int timer;
+    @Getter
+    private static TimerTask task;
+
+    public TimerTask(int timer){
+        this.timer = timer;
+        this.task = this;
+    }
 
     @Override
     public void run() {
+        if(timer == 0){
+            GameRegister.getGame().onStart();
+            this.cancel();
+        }
+        GameRegister.getGame().getPlayers().forEach(p -> {
+            GamePlayerTemplate playerTemplate = (GamePlayerTemplate) p;
+            TitleAPI.sendTitle(playerTemplate.getPlayer(), 5, 10, 5, Messages.TIMER_TITLE, Messages.TIMER_SUBTITLE.replaceAll("%time%", String.valueOf(timer)));
+        });
+        timer--;
+    }
 
+    public static boolean isStart(){
+        return task != null;
     }
 }
