198c4b3af45037d3e946eb984089458a3f4058c3
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 11:31:29 +0200

    Remove player when he leave

--- src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
+++ src/main/java/dev/sdkssh/gameapi/events/ConnectionEvents.java
@@ -4,12 +4,20 @@ import dev.sdkssh.gameapi.register.GameRegister;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ConnectionEvents implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent e){
         if(GameRegister.isRegistered()){
             GameRegister.getGame().addPlayer(e.getPlayer());
+        }
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e){
+        if(GameRegister.isRegistered()){
+            GameRegister.getGame().removePlayer(e.getPlayer().getUniqueId().toString());
         }
     }
 }
