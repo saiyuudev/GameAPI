6f32b1d37155d1604660abbe2d40adb263b66b86
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 15:33:47 +0200

    Remove a property "name"

--- API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
@@ -4,7 +4,6 @@ public class Properties {
     public static Property<Integer> PLAYERS = new Property<>(6);
     @Deprecated
     public static Property<Boolean> MULTIARENA = new Property<>(false);
-    public static Property<String> NAME = new Property<>("Game");
     public static Property<Boolean> BUNGEE = new Property<>(false);
     public static Property<String> BUNGEE_SERVER = new Property<>("");
     public static Property<Boolean> TEAMS = new Property<>(false);
