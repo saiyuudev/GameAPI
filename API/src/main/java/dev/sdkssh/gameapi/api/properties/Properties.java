22b61931bd5e0a594cd4d7c35b29789a0a0ecd75
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 13:59:59 +0200

    Adding new property

--- API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/properties/Properties.java
@@ -10,4 +10,5 @@ public class Properties {
     public static Property<Boolean> TEAMS = new Property<>(false);
     public static Property<Integer> PLAYERS_START = new Property<>(2);
     public static Property<Integer> PLAYERS_FORCE_START = new Property<>(6);
+    public static Property<Integer> START_TIMER = new Property<>(30);
 }
