739dfee9cfd37e600bd770289accd48b1f5fd952
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-07 14:29:35 +0200

    Working on timer and messages

---
+++ API/src/main/java/dev/sdkssh/gameapi/api/message/Messages.java
@@ -0,0 +1,6 @@
+package dev.sdkssh.gameapi.api.message;
+
+public class Messages {
+    public static String TIMER_TITLE = "Start in";
+    public static String TIMER_SUBTITLE = "%timer% seconds !";
+}
---
+++ src/main/java/dev/sdkssh/gameapi/schedulers/TimerTask.java
@@ -0,0 +1,11 @@
+package dev.sdkssh.gameapi.schedulers;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class TimerTask extends BukkitRunnable {
+
+    @Override
+    public void run() {
+
+    }
+}
