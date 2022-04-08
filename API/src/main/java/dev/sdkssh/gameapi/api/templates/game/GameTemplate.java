fa47d0644d2b90bb55be0ff85601bc27f8e07f88
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-08 09:57:21 +0200

    Adding annotation on GameTemplate

--- API/src/main/java/dev/sdkssh/gameapi/api/templates/game/GameTemplate.java
+++ API/src/main/java/dev/sdkssh/gameapi/api/templates/game/GameTemplate.java
@@ -5,6 +5,7 @@ import dev.sdkssh.gameapi.api.templates.player.GamePlayerTemplate;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,26 +20,26 @@ public abstract class GameTemplate<T extends GamePlayerTemplate> {
 
     public abstract void onStart();
 
-    public abstract void onWin(Player p);
+    public abstract void onWin(@NotNull Player p);
 
-    public void win(Player p){
+    public void win(@NotNull Player p){
         setState(GameSTATE.WIN);
         onWin(p);
     }
 
-    public void addPlayer(Player player){
+    public void addPlayer(@NotNull Player player){
         T gameplayer = getNewPlayer(player);
         gameplayer.onJoin();
         players.add(gameplayer);
     }
 
-    public Optional<T> getPlayer(String uuid){
+    public Optional<T> getPlayer(@NotNull String uuid){
         return players.stream().filter(p -> p.getPlayerUUID().equalsIgnoreCase(uuid)).findFirst();
     }
 
-    public abstract T getNewPlayer(Player player);
+    public abstract T getNewPlayer(@NotNull Player player);
 
-    public void removePlayer(String uuid){
+    public void removePlayer(@NotNull String uuid){
         getPlayer(uuid).ifPresent(p -> {
             p.onQuit();
             players.remove(p);
