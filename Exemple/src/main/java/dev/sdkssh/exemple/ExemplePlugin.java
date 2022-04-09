4c063367849bf7f0671cd1128c6c24111ab83271
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-09 09:41:38 +0200

    Register game in exemple

--- Exemple/src/main/java/dev/sdkssh/exemple/ExemplePlugin.java
+++ Exemple/src/main/java/dev/sdkssh/exemple/ExemplePlugin.java
@@ -1,6 +1,13 @@
 package dev.sdkssh.exemple;
 
+import dev.sdkssh.exemple.game.ExempleGame;
+import dev.sdkssh.gameapi.register.GameRegister;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ExemplePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        GameRegister.registerGame(new ExempleGame());
+        super.onEnable();
+    }
 }
