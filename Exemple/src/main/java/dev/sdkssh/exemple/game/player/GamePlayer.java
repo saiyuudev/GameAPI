fda65a5c50b8ccf80331767f5cc75b05ac00a3be
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-09 21:33:45 +0200

    Working on armor in givekit (not finished)

--- Exemple/src/main/java/dev/sdkssh/exemple/game/player/GamePlayer.java
+++ Exemple/src/main/java/dev/sdkssh/exemple/game/player/GamePlayer.java
@@ -1,11 +1,15 @@
 package dev.sdkssh.exemple.game.player;
 
+import dev.sdkssh.exemple.utils.ItemBuilder;
 import dev.sdkssh.gameapi.api.templates.player.GamePlayerTemplate;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class GamePlayer extends GamePlayerTemplate {
@@ -24,6 +28,11 @@ public class GamePlayer extends GamePlayerTemplate {
     }
 
     private void giveKit(Player p){
+        ItemStack[] armors = new ItemStack[4];
+        armors[0] = new ItemBuilder(Material.IRON_HELMET).build();
+        armors[1] = new ItemBuilder(Material.GOLD_CHESTPLATE).addEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 2).build();
+        armors[2] = new ItemBuilder(Material.IRON_HELMET).build();
+        armors[3] = new ItemBuilder(Material.IRON_HELMET).build();
     }
 
     @Override
