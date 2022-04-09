5185150da91485480f624e31d5b8b642c2c296b3
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-09 09:55:33 +0200

    Adding lombok annotation

--- Exemple/src/main/java/dev/sdkssh/exemple/game/player/GamePlayer.java
+++ Exemple/src/main/java/dev/sdkssh/exemple/game/player/GamePlayer.java
@@ -1,10 +1,14 @@
 package dev.sdkssh.exemple.game.player;
 
 import dev.sdkssh.gameapi.api.templates.player.GamePlayerTemplate;
+import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class GamePlayer extends GamePlayerTemplate {
+    @Getter
+    @Setter
     private int life;
 
     public GamePlayer(@NotNull Player player) {
