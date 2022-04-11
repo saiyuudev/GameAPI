22ce4d3e62b763ca18d7572e399576347d7cdb23
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-11 16:29:17 +0200

    Working on game events

--- Exemple/src/main/java/dev/sdkssh/exemple/events/GameEvents.java
+++ Exemple/src/main/java/dev/sdkssh/exemple/events/GameEvents.java
@@ -1,6 +1,26 @@
 package dev.sdkssh.exemple.events;
 
+import dev.sdkssh.exemple.game.player.GamePlayer;
 import dev.sdkssh.gameapi.api.events.GameAPIEvent;
+import dev.sdkssh.gameapi.register.GameRegister;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class GameEvents extends GameAPIEvent {
+    @EventHandler
+    public void onDie(PlayerDeathEvent e){
+        e.setDeathMessage("");
+        e.getDrops().clear();
+        Player p = e.getEntity();
+        GamePlayer gp = getPlayer(p);
+        if(gp.getLife() == 1){
+            //TODO: ELIMINATE PLAYER
+            return;
+        }
+    }
+
+    public GamePlayer getPlayer(Player p){
+        return (GamePlayer) GameRegister.getGame().getPlayer(p.getUniqueId().toString()).get();
+    }
 }
