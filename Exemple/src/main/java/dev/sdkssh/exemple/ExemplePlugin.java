d45be9e1856d3b1f0f415aa1da4e27fb8753f4bf
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-11 16:10:05 +0200

    Init properties

--- Exemple/src/main/java/dev/sdkssh/exemple/ExemplePlugin.java
+++ Exemple/src/main/java/dev/sdkssh/exemple/ExemplePlugin.java
@@ -1,6 +1,7 @@
 package dev.sdkssh.exemple;
 
 import dev.sdkssh.exemple.game.ExempleGame;
+import dev.sdkssh.gameapi.api.properties.Properties;
 import dev.sdkssh.gameapi.register.GameRegister;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,6 +9,10 @@ public class ExemplePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         GameRegister.registerGame(new ExempleGame());
+        Properties.PLAYERS.setValue(4);
+        Properties.PLAYERS_START.setValue(2);
+        Properties.PLAYERS_FORCE_START.setValue(4);
+        Properties.START_TIMER.setValue(90);
         super.onEnable();
     }
 }
