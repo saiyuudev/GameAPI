125374ad1223c7bb8708a2d903cc6ddace673ac3
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-11 16:18:04 +0200

    Adding all condition on event API

--- src/main/java/dev/sdkssh/gameapi/api/events/GameAPIEvent.java
+++ src/main/java/dev/sdkssh/gameapi/api/events/GameAPIEvent.java
@@ -1,5 +1,23 @@
 package dev.sdkssh.gameapi.api.events;
 
+import dev.sdkssh.gameapi.api.states.GameSTATE;
+import dev.sdkssh.gameapi.register.GameRegister;
+import dev.sdkssh.gameapi.schedulers.TimerTask;
+
 public class GameAPIEvent {
-    
+    public boolean isInTimer(){
+        return TimerTask.isStart();
+    }
+
+    public boolean isPlaying(){
+        return GameRegister.getGame().getState() == GameSTATE.INGAME;
+    }
+
+    public boolean isWaiting(){
+        return GameRegister.getGame().getState() == GameSTATE.LOBBY;
+    }
+
+    public boolean isFinish(){
+        return GameRegister.getGame().getState() == GameSTATE.WIN;
+    }
 }
