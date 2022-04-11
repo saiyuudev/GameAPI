be64372ec272f40db070c9691103dac7c20f998d
0xdeadbush <sdk_ssh@protonmail.com>
2022-04-11 16:18:49 +0200

    Implement Listener in event api

--- src/main/java/dev/sdkssh/gameapi/api/events/GameAPIEvent.java
+++ src/main/java/dev/sdkssh/gameapi/api/events/GameAPIEvent.java
@@ -3,8 +3,9 @@ package dev.sdkssh.gameapi.api.events;
 import dev.sdkssh.gameapi.api.states.GameSTATE;
 import dev.sdkssh.gameapi.register.GameRegister;
 import dev.sdkssh.gameapi.schedulers.TimerTask;
+import org.bukkit.event.Listener;
 
-public class GameAPIEvent {
+public class GameAPIEvent implements Listener {
     public boolean isInTimer(){
         return TimerTask.isStart();
     }
